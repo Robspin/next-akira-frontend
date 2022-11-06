7db3578cdcd83f0d11659038a3549725b02c96c5
Robin Steeman <robin@tracefy.com>
2022-11-06 20:44:39 +0100

    dark background and image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,19 @@
+import Image from "next/image"
+
 export default function Home({ trades }: { trades: any[] }) {
-    console.log(trades)
 
     return (
-      <h1>
+      <div className="h-screen w-screen bg-black flex items-center flex-col">
+          <div className="py-20">
+            <Image src="/logo.png" alt="akira-logo" height={175} width={300} />
+          </div>
           {trades.map(trade => (
-              <div className="flex">
+              <div className="text-white flex">
                   <div className="mr-4">{trade.createdAt}</div>
                   <div>{trade.side}</div>
               </div>
           ))}
-      </h1>
+      </div>
     )
 }
 
